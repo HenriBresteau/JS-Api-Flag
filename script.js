a97426f8132bf69241c608a8ea983a4f9331c797
henriB <henri.bretseau@gmail.com>
2020-11-04 21:43:13 +0100

    searchInput

--- script.js
+++ script.js
@@ -31,6 +31,12 @@ const showCountries = async() => {
 }
 showCountries();
 
+// INPUT SETUP
+searchInput.addEventListener( 'input', (e) => {
+    searchTerm = e.target.value;
+    showCountries();
+})
+
 function numberFilter(x){
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 }
