0984778be27417802673ba5fd37dcc7fecac093f
henriB <henri.bretseau@gmail.com>
2020-11-04 21:30:44 +0100

    showCountries

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
+const searchInput = document.getElementById('search');
+const results = document.getElementById('results');
+
 let countries ;
+let searchTerm ='';
 
 // API REQUEST
 const fecthContries = async() => {
     countries = await fetch(
         'https://restcountries.eu/rest/v2/all?fields=name;population;flag'
     ).then(res=> res.json());
-    console.log(countries);
 };
 
-fecthContries();
+const showCountries = async() => {
+    await fecthContries();
+    console.log(countries);
+    results.innerHTML= (
+        countries.filter(country => country.name.toLowerCase().includes(searchTerm.toLowerCase())).map(country => (
+            `
+            <li class="country-item">
+                <img class="country-flag" src="${country.flag}" />
+                <h3 class="country-name"> ${country.name} </h3>
+                <div class="country-info">
+                    <h2 class="country-population"> ${country.population} </h2>
+                    <h5 class="country-population-text"> Habitants</h5>
+                </div>
+            </li>
+            `
+        )).join('')
+    );
+}
+showCountries();
